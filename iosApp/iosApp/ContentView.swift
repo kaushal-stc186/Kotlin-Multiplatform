94d02646cf1af7e4b339cd0fcfcb9f405e3398b0
kaushalSTC <kaushal.kumar@seventhtriangle.com>
2026-02-13 18:22:04 +0530

    button implementation

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -6,6 +6,18 @@ struct ContentView: View {
         VStack(spacing: 50){
             Text("hello world, ios")
             Text("Helloooooooo").foregroundColor(Color(hex: 0xFF5733)).font(.system(size: 80, weight: .bold))
+            Button("Click Me Text") {
+                print("Button Clicked")
+            }
+            Button(action: {
+                print("Hello World Button Pressed")
+            }) {
+                    Text("Hello World").foregroundColor(Color(hex: 0xFFFFFF))
+            }.buttonStyle(.plain)
+            .frame(width: 100, height: 100)
+            .background(Color.green)
+            .foregroundColor(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 50))
         }.frame(maxWidth: .infinity, maxHeight: .infinity).background(Color(hex: 0xFF7821))
     }
 }
